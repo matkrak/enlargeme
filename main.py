919ed1e6374a4dab773399383181879ab0e4a5eb
Mateusz Krakowski <kraaaki01@gmail.com>
2017-11-21 18:34:11 +0100

    merged logger branch, missing main file to be fixed

---
+++ main.py
@@ -0,0 +1,7 @@
+# TODO forgot to push this file to remote, use this tmp file and push another commit on remote
+
+from tests import *
+
+if __name__ == '__main__':
+    compareFiltersNo()
+    compareMatrixScalar()
--- tests.py
+++ tests.py
@@ -18,11 +18,11 @@ def compareMatrixScalar():
 
 def compareFiltersNo():
     rbms = []
-    for i in range(5):
+    for i in range(6):
         rbms.append((CCRBM.CCRBM(64, 64, i * 10 + 10, (5, 5)), str(i * 10 + 10)))
         rbms[i][0].dh.readBrainWebData(resize=True, shape=(64, 64))
         rbms[i][0].dh.normalize()
-		rbms[i][0].contrastiveDivergence(100, 1e-6, 10, 20)
+        rbms[i][0].contrastiveDivergence(100, 1e-6, 10, 20)
         rbms[i][0].saveToFile('compare' + rbms[i][1])
 
 

6ef89e4e28bee12c82d5f8a5c0efaa6d7ebd904a
Mateusz Krakowski <kraaaki01@gmail.com>
2017-11-28 14:32:43 +0100

    refactored datahandler for new data

--- DataHandler.py
+++ DataHandler.py
@@ -12,6 +12,7 @@ MNIST_TRAIN_LABELS = 'data/train-labels-idx1-ubyte/data'
 MNIST_TRAIN_IMAGES = 'data/train-images-idx3-ubyte/data'
 BRAINWEB_IMAGES = 'data/BrainWeb_data.mat'
 
+BRAINWEB_NPY = 'data/t1_filtered.npy'
 
 class DataHandler:
     def __init__(self):
@@ -20,12 +21,20 @@ class DataHandler:
 
         self.shape = None
 
-        self.normalParams = {}
+        self.normalParams = dict()
+        self.normalParams['normalized'] = None
 
         self.tr_size = None
         self.te_size = None
 
         self.current_iter = None
+
+    def __str__(self):
+        res = "images shape: (H, W) = {}, train set # {}, test set # {}, normalized: {}".format(self.shape,
+                                                                                                   self.tr_size,
+                                                                                                   self.te_size,
+                                                                                                   self.normalParams['normalized'])
+        return res
 
 
 
@@ -41,16 +50,23 @@ class DataHandler:
     #             self.labels = np.fromfile(flab, dtype=np.int8)
 
     def readBrainWebData(self, img_path=BRAINWEB_IMAGES, resize=None, shape=None, train_test_ratio=20):
+        """
+        Read BrainWeb data mat file. Create test and train sets.
+        :param img_path: Path to mat file
+        :param resize: resize data?
+        :param shape: tuple, shape should be (Height, Width).
+        :param train_test_ratio: how many images in train/test set?
+        """
         #hardcoded for now
         matfile = sio.loadmat(img_path)
 
         data = matfile.get('dataset_T1')
-        if resize is None:
+        if resize is not True:
             dims = (data.shape[2], data.shape[0], data.shape[1])
         else:
             if shape is None:
-                logger.error('[readBrainWebData] When resize is not None size arg must be provided!')
-                raise ValueError('When resize is not None size arg must be provided!')
+                logger.error('[readBrainWebData] When resize is not None shape arg must be provided!')
+                raise ValueError('When resize is not None shape arg must be provided!')
             dims = (data.shape[2], shape[0], shape[1])
 
         self.te_size = int(data.shape[2]/(train_test_ratio + 1))
@@ -62,7 +78,7 @@ class DataHandler:
         shuffling = list(range(data.shape[2]))
 
         img_no = 0
-        if resize is None:
+        if resize is not True:
             while img_no < self.tr_size:
                 self.train.append(data[:, :, shuffling.pop(np.random.randint(0, len(shuffling)))])
                 img_no += 1
@@ -72,21 +88,66 @@ class DataHandler:
                 img_no += 1
         else:
             while img_no < self.tr_size:
-                self.train.append(np.asarray(Image.fromarray(data[:, :, shuffling.pop(np.random.randint(0, len(shuffling)))]).resize(shape)))
+                self.train.append(np.asarray(Image.fromarray(data[:, :, shuffling.pop(np.random.randint(0, len(shuffling)))]).resize((shape[1], shape[0]))))
                 img_no += 1
 
             while img_no < dims[0]:
-                self.test.append(np.asarray(Image.fromarray(data[:, :, shuffling.pop(np.random.randint(0, len(shuffling)))]).resize(shape)))
+                self.test.append(np.asarray(Image.fromarray(data[:, :, shuffling.pop(np.random.randint(0, len(shuffling)))]).resize((shape[1], shape[0]))))
                 img_no += 1
 
         self.shape = (dims[1], dims[2])
+        self.normalParams['normalized'] = False
 
-        print('Loaded Brainweb data. Image size: ' + str(self.shape))
+        print('Loaded Brainweb data. Image shape: (H, W) = ' + str(self.shape))
         print('Trainset size: ' + str(self.tr_size))
         print('Tetset size:   ' + str(self.te_size))
-        logger.info('Loaded Brainweb data. Image size: {}, train set size: {}, test set size: {}'.format(self.shape, self.tr_size, self.te_size))
+        logger.info('Loaded Brainweb data. Image shape: (H, W) = {}, train set size: {}, test set size: {}'.format(self.shape, self.tr_size, self.te_size))
 
-    def normalize(self):
+    def readnpy(self, img_path=BRAINWEB_NPY, resize=None, shape=None, train_test_ratio=20): # TODO double check normalization (make it +-1)
+        data = np.load(img_path)
+
+        if resize is not True:
+            dims = data.shape
+        else:
+            if shape is None:
+                logger.error('[readnpy] When resize is not None shape arg must be provided!')
+                raise ValueError('When resize is not None shape arg must be provided!')
+            dims = (data.shape[0], shape[0], shape[1])
+
+        self.te_size = int(dims[0] / (train_test_ratio + 1))
+        self.tr_size = dims[0] - self.te_size
+
+        shuffling = list(range(dims[0]))
+        self.train = []
+        self.test = []
+
+        if resize is not True:
+            for i in range(self.tr_size):
+                self.train.append(data[shuffling.pop(np.random.randint(0, len(shuffling))), :, :])
+            for i in range(self.te_size):
+                self.test.append(data[shuffling.pop(np.random.randint(0, len(shuffling))), :, :])
+
+        else:
+            for i in range(self.tr_size):
+                self.train.append(np.asarray(Image.fromarray(data[shuffling.pop(np.random.randint(0, len(shuffling))), :, :]).resize((shape[1], shape[0]))))
+            for i in range(self.te_size):
+                self.test.append(np.asarray(Image.fromarray(data[shuffling.pop(np.random.randint(0, len(shuffling))), :, :]).resize((shape[1], shape[0]))))
+
+
+        self.shape = (dims[1], dims[2])
+        self.normalParams['normalized'] = False
+
+        print('Loaded numpy data. Image shape: (v, h) = ' + str(self.shape))
+        print('Train set size: ' + str(self.tr_size))
+        print('Tet set size:   ' + str(self.te_size))
+        logger.info(
+            'Loaded numpy data. Image shape: (v, h) = {}, train set size: {}, test set size: {}'.format(self.shape,
+                                                                                                   self.tr_size,
+                                                                                                   self.te_size))
+
+    def normalize(self, force=False):
+        if self.normalParams['normalized'] and not force:
+            raise RuntimeWarning('Data already normalized! Use force is necessary ;)')
 
         tmp_tr = np.ndarray(shape=(self.tr_size, *self.shape))
         tmp_te = np.ndarray(shape=(self.te_size, *self.shape))
@@ -101,7 +162,7 @@ class DataHandler:
             self.normalParams['means_te'].append(self.test[i].mean())
             self.test[i] = self.test[i] - self.normalParams['means_te'][i]
 
-        # compute std for each dataset, remember it and divide!
+        # compute std for each data set, remember it and divide!
         for i in range(self.tr_size):
             tmp_tr[i, :, :] = np.copy(self.train[i])
         self.normalParams['std_tr'] = tmp_tr.std()
@@ -116,7 +177,25 @@ class DataHandler:
         for i in range(self.te_size):
             self.test[i] = self.test[i] / self.normalParams['std_te']
 
-        logger.info('Successfully normalized data from DataHandler object.')
+        self.normalParams['normalized'] = True
+        logger.info('Successfully normalized data object.')
+
+    def getimg(self, idx, dataset='train', normalize=False):
+        if dataset not in ('train', 'test'):
+            raise ValueError('Only test or train set here')
+
+        usetrainset = True if dataset == 'train' else False
+        im = np.copy(self.train[idx] if usetrainset else self.test[idx])
+
+        if normalize and self.normalParams['normalized']:
+                im *= self.normalParams['std_tr' if usetrainset else 'std_tr']
+                im += self.normalParams['means_tr' if usetrainset else 'means_te'][idx]
+
+        return im
+
+    def imshow(self, idx, dataset='train', normalize=True):
+        Image.fromarray(np.ndarray.astype(self.getimg(idx, dataset, normalize), 'int8')).show()
+
 
 if __name__ == '__main__':
     print('Tell me what to do')
